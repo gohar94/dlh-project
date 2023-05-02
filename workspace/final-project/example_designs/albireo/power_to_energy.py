357840ecd80b0887e3bdecad9a6e031cbdf1efe6
Gohar Irfan Chaudhry <girfan@mit.edu>
2023-05-02 16:51:30 -0400

    caches

--- workspace/final-project/example_designs/albireo/power_to_energy.py
+++ workspace/final-project/example_designs/albireo/power_to_energy.py
@@ -39,34 +39,44 @@ def prefix(x, dimension=1):
 
 
 # Constants
+alexnet_macs                = 834600961
+total_energy_joules         = 2.90 * MILLI
+total_latency_seconds       = 0.13 * MILLI 
+mrr_power_percent           = 0.331
+mzm_power_percent           = 0.152
+weight_caches_power_percent = 0.0013
+num_mrrs                    = 2 * 9 * 5
+num_mzms                    = 9
+num_weight_caches           = 9
+
 print("CONSTANTS:")
-alexnet_macs          = 834600961
 print(f"{'alexnet_macs':<25} {prefix(alexnet_macs):>11}")
-total_energy_joules   = 2.90 * MILLI
 print(f"{'total_energy_joules':<25} {prefix(total_energy_joules):>10}J")
-total_latency_seconds = 0.13 * MILLI 
 print(f"{'total_latency_seconds':<25} {prefix(total_latency_seconds):>10}s")
-mrr_power_percent     = 0.331
 print(f"{'mrr_power_percent':<25} {mrr_power_percent * 100:>10}%")
-mzm_power_percent     = 0.152
 print(f"{'mzm_power_percent':<25} {mzm_power_percent * 100:>10}%")
-num_mrrs              = 2 * 9 * 5
+print(f"{'weight_caches_power_percent':<25} {weight_caches_power_percent * 100:>8}%")
 print(f"{'num_mrrs':<25} {num_mrrs:>11}")
-num_mzms              = 9
 print(f"{'num_mzms':<25} {num_mzms:>11}")
+print(f"{'num_weight_caches':<25} {num_weight_caches:>11}")
 
 
 # Derived values
+energy_per_mac           = total_energy_joules / alexnet_macs
+energy_all_mrrs          = energy_per_mac * mrr_power_percent
+energy_per_mrr           = energy_all_mrrs / num_mrrs
+energy_all_mzms          = energy_per_mac * mzm_power_percent
+energy_per_mzm           = energy_all_mzms / num_mzms
+energy_all_weight_caches = energy_per_mac * weight_caches_power_percent
+energy_per_weight_cache  = energy_all_weight_caches / num_weight_caches
+total_power_watts        = total_energy_joules / total_latency_seconds
+
 print("\nDERIVED:")
-energy_per_mac = total_energy_joules / alexnet_macs
 print(f"{'energy_per_mac':<25} {prefix(energy_per_mac):>10}J")
-energy_all_mrrs = energy_per_mac * mrr_power_percent
 print(f"{'energy_all_mrrs':<25} {prefix(energy_all_mrrs):>10}J")
-energy_per_mrr = energy_all_mrrs / num_mrrs
 print(f"{'energy_per_mrr':<25} {prefix(energy_per_mrr):>10}J")
-energy_all_mzms = energy_per_mac * mzm_power_percent
 print(f"{'energy_all_mzms':<25} {prefix(energy_all_mzms):>10}J")
-energy_per_mzm = energy_all_mzms / num_mzms
+print(f"{'energy_all_weight_caches':<25} {prefix(energy_all_weight_caches):>10}J")
 print(f"{'energy_per_mzm':<25} {prefix(energy_per_mzm):>10}J")
-total_power_watts = total_energy_joules / total_latency_seconds
+print(f"{'energy_per_weight_cache':<25} {prefix(energy_per_weight_cache):>10}J")
 print(f"{'total_power_watts':<25} {prefix(total_power_watts):>10}W")
