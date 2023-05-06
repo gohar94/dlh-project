fc9528e82957c5112b2f4201e2906968d9258eab
VMS-6511 <suriyaku@gmail.com>
2023-05-06 15:04:20 -0400

    updates to energy calculatio

--- workspace/final-project/example_designs/albireo/energy_calculation.py
+++ workspace/final-project/example_designs/albireo/energy_calculation.py
@@ -50,9 +50,10 @@ num_mzms                    = 9
 num_weight_caches           = 9
 TIA_power_percent           = 0.0062
 ADC_power_percent           = 0.058
+DAC_power_percent           = 0.349
 num_TIA                     = 45
 num_ADC                     = 9
-
+num_DAC                     = 1
 
 
 print("CONSTANTS:")
@@ -67,36 +68,42 @@ print(f"{'num_mzms':<25} {num_mzms:>11}")
 print(f"{'num_weight_caches':<25} {num_weight_caches:>11}")
 print(f"{'TIA_power_percent':<25} {TIA_power_percent * 100:>10}%")
 print(f"{'ADC_power_percent':<25} {ADC_power_percent * 100:>10}%")
+print(f"{'DAC_power_percent':<25} {DAC_power_percent * 100:>10}%")
 print(f"{'num_TIA':<25} {num_TIA:>11}")
 print(f"{'num_ADC':<25} {num_ADC:>11}")
 
 
 # Derived values
-energy_per_mac           = total_energy_joules / alexnet_macs
-energy_all_mrrs          = energy_per_mac * mrr_power_percent
-energy_per_mrr           = energy_all_mrrs / num_mrrs
-energy_all_mzms          = energy_per_mac * mzm_power_percent
-energy_per_mzm           = energy_all_mzms / num_mzms
-energy_all_weight_caches = energy_per_mac * weight_caches_power_percent
-energy_per_weight_cache  = energy_all_weight_caches / num_weight_caches
+energy_per_compute       = total_energy_joules / alexnet_macs
 total_power_watts        = total_energy_joules / total_latency_seconds
-energy_all_TIAs          = energy_per_mac * TIA_power_percent
-energy_per_TIA           = energy_all_TIAs / num_TIA
-energy_all_ADC           = energy_per_mac * ADC_power_percent
-energy_per_mzm           = energy_all_ADC / num_ADC
 
+energy_all_mrrs          = energy_per_compute * mrr_power_percent
+energy_all_mzms          = energy_per_compute * mzm_power_percent
+energy_all_weight_caches = energy_per_compute * weight_caches_power_percent
+energy_all_TIAs          = energy_per_compute * TIA_power_percent
+energy_all_ADC           = energy_per_compute * ADC_power_percent
+energy_all_DAC           = energy_per_compute * DAC_power_percent
+
+energy_per_mrr           = energy_all_mrrs / num_mrrs
+energy_per_mzm           = energy_all_mzms / num_mzms
+energy_per_weight_cache  = energy_all_weight_caches / num_weight_caches
+energy_per_TIA           = energy_all_TIAs / num_TIA
+energy_per_ADC           = energy_all_ADC / num_ADC
+energy_per_DAC           = energy_all_DAC / num_DAC
 
 
 print("\nDERIVED:")
-print(f"{'energy_per_mac':<25} {prefix(energy_per_mac):>10}J")
+print(f"{'energy_per_compute':<25} {prefix(energy_per_compute):>10}J")
 print(f"{'energy_all_mrrs':<25} {prefix(energy_all_mrrs):>10}J")
 print(f"{'energy_per_mrr':<25} {prefix(energy_per_mrr):>10}J")
 print(f"{'energy_all_mzms':<25} {prefix(energy_all_mzms):>10}J")
-print(f"{'energy_all_weight_caches':<25} {prefix(energy_all_weight_caches):>10}J")
 print(f"{'energy_per_mzm':<25} {prefix(energy_per_mzm):>10}J")
+print(f"{'energy_all_weight_caches':<25} {prefix(energy_all_weight_caches):>10}J")
 print(f"{'energy_per_weight_cache':<25} {prefix(energy_per_weight_cache):>10}J")
 print(f"{'energy_all_TIAs':<25} {prefix(energy_all_TIAs):>10}J")
 print(f"{'energy_per_TIAs':<25} {prefix(energy_per_TIA):>10}J")
 print(f"{'energy_all_ADC':<25} {prefix(energy_all_ADC):>10}J")
-print(f"{'energy_per_ADC':<25} {prefix(energy_per_mzm):>10}J")
+print(f"{'energy_per_ADC':<25} {prefix(energy_per_ADC):>10}J")
+print(f"{'energy_all_DAC':<25} {prefix(energy_all_DAC):>10}J")
+print(f"{'energy_per_DAC':<25} {prefix(energy_per_DAC):>10}J")
 print(f"{'total_power_watts':<25} {prefix(total_power_watts):>10}W")
