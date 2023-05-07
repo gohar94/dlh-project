6d643665224f4ab6e14dc6a8190684b420621093
Abhiram Iyer <abhi.iyer.ai@gmail.com>
2023-05-07 17:50:56 -0400

    tons of updates

---
+++ workspace/final-project/scripts/utils.py
@@ -0,0 +1,68 @@
+import re 
+import pickle
+
+def get_numbers_from_summary(string):
+    keys = [
+        'GLOPs',
+        'Utilization', 
+        'Cycles', 
+        'Energy',
+        'EDP(J*cycle)', 
+        'Area',
+        'Computes',
+        'pJ/Compute omacs',
+        'pJ/Compute psumBuffer',
+        'pJ/Compute plcu_dummy_buffer',
+        'pJ/Compute weight_cache',
+        'pJ/Compute dummy_buffer',
+        'pJ/Compute shared_glb',
+        'pJ/Compute DRAM',
+        'pJ/Compute A2D_NoC',
+        'pJ/Compute D2A_NoC',
+        'pJ/Compute DRAM <==> shared_glb',
+        'pJ/Compute dummy_buffer <==> weight_cache',
+        'pJ/Compute plcu_dummy_buffer <==> psumBuffer',
+        'pJ/Compute psumBuffer <==> omacs',
+        'pJ/Compute weight_cache <==> plcu_dummy_buffer',
+        'pJ/Compute Total',
+    ]
+    
+    patterns = re.findall(r"[:=]\s*[-+]?[.]?[\d]+(?:,\d\d\d)*[\.]?\d*(?:[eE][-+]?\d+)?", string)
+    
+    numbers = [float(
+        re.search(r"[-+]?[.]?[\d]+(?:,\d\d\d)*[\.]?\d*(?:[eE][-+]?\d+)?", pattern).group()
+    ) for pattern in patterns]
+    
+    assert len(keys) == len(numbers)
+    
+    return dict(zip(keys, numbers))
+
+
+def save_data(obj, fname):
+    with open(fname, 'wb') as file:
+        pickle.dump(obj, file)
+
+        
+def load_data(fname):
+    with open(fname, 'rb') as file:
+        return pickle.load(file)
+    
+    
+def extract_numbers(stats_by_layer, fname):
+    all_stats = []
+    
+    for stats in stats_by_layer:
+        all_stats.append(get_numbers_from_summary(stats[stats.find("Summary Stats"):]))
+        
+    save_data(all_stats, fname)
+    
+    
+def save_loop_nests(loop_nests, fname):
+    string = ''
+    for i, loop in enumerate(loop_nests):
+        string += 'Layer {}\n\n'.format(i)
+        string += loop
+        string += '\n\n\n\n'
+    
+    with open(fname, 'w') as file:
+        file.write(string)
