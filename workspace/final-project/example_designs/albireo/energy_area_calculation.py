2ac019fefaffa1482d5c44c03630151f5ad09298
Gohar Irfan Chaudhry <girfan@mit.edu>
2023-05-08 18:43:55 -0400

    Updated area and energy

--- workspace/final-project/example_designs/albireo/energy_area_calculation.py
+++ workspace/final-project/example_designs/albireo/energy_area_calculation.py
@@ -39,6 +39,17 @@ def prefix(x, dimension=1):
     return "%.3g %s" % (x * 10**(-l + mod), " kMGTPEZYyzafpnµm"[div])
 
 
+def area_milli_to_micro(x):
+    """Given an area in mm^2, convert it to um^2.
+    """
+    return x * 1000000
+
+
+def percent_to_decimal(x):
+    """Convert a percentage to decimal.
+    """
+    return x/100
+
 # Constants
 alexnet_macs                = 834600961
 total_energy_joules         = 2.90 * MILLI
@@ -46,32 +57,38 @@ total_latency_seconds       = 0.13 * MILLI
 mrr_power_percent           = 0.331
 mzm_power_percent           = 0.152
 weight_caches_power_percent = 0.0013
-num_mrrs                    = 2 * 9 * 5
-num_mzms                    = 9
-num_weight_caches           = 9
+num_plcg                    = 9
+num_plcu                    = 3
+num_mrr                    = num_plcg * num_plcu * 2 * 9 * 5
+num_mzm                    = num_plcu * 9
+num_weight_caches           = num_plcg * 1
 TIA_power_percent           = 0.0062
 ADC_power_percent           = 0.058
 DAC_power_percent           = 0.349
 num_TIA                     = 45
-num_ADC                     = 9
+num_ADC                     = num_plcg * 1
 num_DAC                     = 1
+num_AWG                     = num_plcg
+num_star_coupler            = num_plcg * num_plcu
 
 
-print("CONSTANTS:")
+print("Energy:")
 print(f"{'alexnet_macs':<25} {prefix(alexnet_macs):>11}")
 print(f"{'total_energy_joules':<25} {prefix(total_energy_joules):>10}J")
 print(f"{'total_latency_seconds':<25} {prefix(total_latency_seconds):>10}s")
 print(f"{'mrr_power_percent':<25} {mrr_power_percent * 100:>10}%")
 print(f"{'mzm_power_percent':<25} {mzm_power_percent * 100:>10}%")
 print(f"{'weight_caches_power_percent':<25} {weight_caches_power_percent * 100:>8}%")
-print(f"{'num_mrrs':<25} {num_mrrs:>11}")
-print(f"{'num_mzms':<25} {num_mzms:>11}")
+print(f"{'num_mrr':<25} {num_mrr:>11}")
+print(f"{'num_mzm':<25} {num_mzm:>11}")
 print(f"{'num_weight_caches':<25} {num_weight_caches:>11}")
 print(f"{'TIA_power_percent':<25} {TIA_power_percent * 100:>10}%")
 print(f"{'ADC_power_percent':<25} {ADC_power_percent * 100:>10}%")
 print(f"{'DAC_power_percent':<25} {DAC_power_percent * 100:>10}%")
 print(f"{'num_TIA':<25} {num_TIA:>11}")
 print(f"{'num_ADC':<25} {num_ADC:>11}")
+print(f"{'num_AWG':<25} {num_AWG:>11}")
+print(f"{'num_star_coupler':<25} {num_star_coupler:>11}")
 
 
 # Derived values
@@ -85,55 +102,66 @@ energy_all_TIAs          = energy_per_compute * TIA_power_percent
 energy_all_ADC           = energy_per_compute * ADC_power_percent
 energy_all_DAC           = energy_per_compute * DAC_power_percent
 
-energy_per_mrr           = energy_all_mrrs / num_mrrs
-energy_per_mzm           = energy_all_mzms / num_mzms
+energy_per_mrr           = energy_all_mrrs / num_mrr
+energy_per_mzm           = energy_all_mzms / num_mzm
 energy_per_weight_cache  = energy_all_weight_caches / num_weight_caches
 energy_per_TIA           = energy_all_TIAs / num_TIA
 energy_per_ADC           = energy_all_ADC / num_ADC
 energy_per_DAC           = energy_all_DAC / num_DAC
 
 
-print("\nDERIVED:")
-print(f"{'energy_per_compute':<25} {prefix(energy_per_compute):>10}J")
-print(f"{'energy_all_mrrs':<25} {prefix(energy_all_mrrs):>10}J")
-print(f"{'energy_per_mrr':<25} {prefix(energy_per_mrr):>10}J")
-print(f"{'energy_all_mzms':<25} {prefix(energy_all_mzms):>10}J")
-print(f"{'energy_per_mzm':<25} {prefix(energy_per_mzm):>10}J")
-print(f"{'energy_all_weight_caches':<25} {prefix(energy_all_weight_caches):>10}J")
-print(f"{'energy_per_weight_cache':<25} {prefix(energy_per_weight_cache):>10}J")
-print(f"{'energy_all_TIAs':<25} {prefix(energy_all_TIAs):>10}J")
-print(f"{'energy_per_TIAs':<25} {prefix(energy_per_TIA):>10}J")
-print(f"{'energy_all_ADC':<25} {prefix(energy_all_ADC):>10}J")
-print(f"{'energy_per_ADC':<25} {prefix(energy_per_ADC):>10}J")
-print(f"{'energy_all_DAC':<25} {prefix(energy_all_DAC):>10}J")
-print(f"{'energy_per_DAC':<25} {prefix(energy_per_DAC):>10}J")
+print("")
 print(f"{'total_power_watts':<25} {prefix(total_power_watts):>10}W")
-
-
-def percent_to_decimal(x):
-    return x/100
-
-def area_to_base(area, units):
-    return area * (units * units)
-
-
-total_albireo_area = area_to_base(124.66, MILLI)
-
-mzi_area = total_albireo_area * percent_to_decimal(3.7)
-dac_area = total_albireo_area * percent_to_decimal(0.03)
-adc_area = total_albireo_area * percent_to_decimal(0.4)
-mrr_area = total_albireo_area * percent_to_decimal(0.8)
+print(f"{'energy_all_mrrs':<25} {prefix(energy_all_mrrs):>10}J")
+print(f"{'energy_all_mzms':<25} {prefix(energy_all_mzms):>10}J")
+print(f"{'energy_all_weight_caches':<25} {prefix(energy_all_weight_caches):>10}J")
+print(f"{'energy_all_TIAs':<25} {prefix(energy_all_TIAs):>10}J")
+print(f"{'energy_all_ADC':<25} {prefix(energy_all_ADC):>10}J")
+print(f"{'energy_all_DAC':<25} {prefix(energy_all_DAC):>10}J")
 
 print()
-print("MZI area in mm^2: {:.4f}".format(area_to_base(mzi_area, 1/MILLI)))
-print("DAC area in mm^2: {:.4f}".format(area_to_base(dac_area, 1/MILLI)))
-print("ADC area in mm^2: {:.4f}".format(area_to_base(adc_area, 1/MILLI)))
-print("MRR area in mm^2: {:.4f}".format(area_to_base(mrr_area, 1/MILLI)))
-print()
+print(f"{'energy_per_compute':<25} {prefix(energy_per_compute):>10}J")
+print(f"{'energy_per_mrr':<25} {prefix(energy_per_mrr):>10}J")
+print(f"{'energy_per_mzm':<25} {prefix(energy_per_mzm):>10}J")
+print(f"{'energy_per_weight_cache':<25} {prefix(energy_per_weight_cache):>10}J")
+print(f"{'energy_per_TIAs':<25} {prefix(energy_per_TIA):>10}J")
+print(f"{'energy_per_ADC':<25} {prefix(energy_per_ADC):>10}J")
+print(f"{'energy_per_DAC':<25} {prefix(energy_per_DAC):>10}J")
+
+
+total_albireo_area    = 124.66 * MILLI
+mzm_area              = total_albireo_area * percent_to_decimal(3.7)
+dac_area              = total_albireo_area * percent_to_decimal(0.03)
+adc_area              = total_albireo_area * percent_to_decimal(0.4)
+mrr_area              = total_albireo_area * percent_to_decimal(0.8)
+awg_area              = total_albireo_area * percent_to_decimal(72)
+star_coupler_area     = total_albireo_area * percent_to_decimal(17)
+
 
 print()
-print("MZI area in um^2: {:.4f}".format(area_to_base(mzi_area, 1/MICRO)))
-print("DAC area in um^2: {:.4f}".format(area_to_base(dac_area, 1/MICRO)))
-print("ADC area in um^2: {:.4f}".format(area_to_base(adc_area, 1/MICRO)))
-print("MRR area in um^2: {:.4f}".format(area_to_base(mrr_area, 1/MICRO)))
+print("\nArea:")
+print("Total area in um^2: {:.4f}".format(area_milli_to_micro(total_albireo_area)))
+print("Total MZM area in um^2: {:.4f}".format(area_milli_to_micro(mzm_area)))
+print("Total DAC area in um^2: {:.4f}".format(area_milli_to_micro(dac_area)))
+print("Total ADC area in um^2: {:.4f}".format(area_milli_to_micro(adc_area)))
+print("Total MRR area in um^2: {:.4f}".format(area_milli_to_micro(mrr_area)))
+print("Total AWG area in um^2: {:.4f}".format(area_milli_to_micro(awg_area)))
+print("Total star coupler area in um^2: {:.4f}".format(area_milli_to_micro(star_coupler_area)))
+
+
+per_mzm_area          = mzm_area / num_mzm
+per_mrr_area          = mrr_area / num_mrr
+per_dac_area          = dac_area / num_DAC
+per_adc_area          = adc_area / num_ADC
+per_awg_area          = awg_area / num_AWG
+per_star_coupler_area = star_coupler_area / num_star_coupler
+
+
+print("")
+print("Each MZM area in um^2: {:.4f}".format(area_milli_to_micro(per_mzm_area)))
+print("Each DAC area in um^2: {:.4f}".format(area_milli_to_micro(per_dac_area)))
+print("Each ADC area in um^2: {:.4f}".format(area_milli_to_micro(per_adc_area)))
+print("Each MRR area in um^2: {:.4f}".format(area_milli_to_micro(per_mrr_area)))
+print("Each AWG area in um^2: {:.4f}".format(area_milli_to_micro(per_awg_area)))
+print("Each star coupler area in um^2: {:.4f}".format(area_milli_to_micro(per_star_coupler_area)))
 print()
